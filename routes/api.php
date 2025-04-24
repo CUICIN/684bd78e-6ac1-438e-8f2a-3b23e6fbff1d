1d1eae40e473b48743e12411c3043d8b78267d28
t.ikeda <t.ikeda@aipass.inc>
2025-04-24 09:55:47 +0900

    feat: 管理者によるユーザーリスト操作機能を作成

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
 <?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', [UserController::class, 'listUsersWithPosts']);
+Route::post('/user', [UserController::class, 'createUser']);
+Route::post('/user-block/{id}', [UserController::class, 'blockUser']);
+Route::post('/user/{id}', [UserController::class, 'deleteUser']);
